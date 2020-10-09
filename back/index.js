2352a22ff9bdee5411ec563e4ed3ee869bce868b
Jacqueline Nguyen <jacqueline.nguyen.41@gmail.com>
2020-10-09 02:09:02 +0200

    added dependencies

--- back/index.js
+++ back/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
-const port = 4000;
+const connection = require ('./conf')
+const port = 5000;
 
 const app = express();
 
+// Entry point of server 'http://localhost:5000/'
+app.get('/', (req, res) => {
+    res.send("Hello there, I'm root").status(200)
+})
+
+app.listen(port, err => {
+    if (err){ throw new Error('Something bad happened :(')}
+    console.log(`Server is listening on ${port}`)
+}) 
