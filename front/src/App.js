9bcec597e97d35d022c6fc92f1e88b18032e543c
FlorentCmb <AS0D@protonmail.ch>
2020-11-03 16:23:09 +0100

    Add comments to App

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
+// Librairies
 import React from 'react'
 import { Switch, Route} from "react-router-dom"
-
+// Components
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
 import Banner from './components/Banner/Banner'
 import RecipeSearchBar from './components/RecipeSearchBar/RecipeSearchBar'
+import Footer from './components/Footer/Footer'
+// Screens
 import TrendyRecipes from './components/TrendyRecipes/TrendyRecipes'
 import Recipes from './components/Recipes/Recipes'
-import Footer from './components/Footer/Footer'
-
+// Style
 import './App.css';
 
 const App = () => {
