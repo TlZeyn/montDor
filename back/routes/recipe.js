ab5d3b82baf355df9f460fb1fcbac4918dd37afd
Jacqueline Nguyen <jacqueline.nguyen.41@gmail.com>
2020-11-06 16:48:55 +0100

    added the query to get all recipes in recipe

--- back/routes/recipe.js
+++ back/routes/recipe.js
@@ -2,5 +2,17 @@ const express = require('express')
 const router = express.Router()
 const connection = require('../conf')
 
+router.get('/', (req, res) => {
+    //print all recipes in homepage : http://localhost:5000/accueil/
+    connection.query(
+        'SELECT rcp_title AS title, rcp_nbEater AS nbEater, rcp_photo AS photo FROM t_recipe_rcp', (err, results) => {
+            if (err) {
+                res.status(500).send('Erreur lors de la récupération des recettes');
+            } else {
+                res.json(results);
+            }
+        })
+})
+
 
 module.exports = router;
