3dfc3d37cb4b1e083e72472cb8da6ec4b33b83d3
FlorentCmb <AS0D@protonmail.ch>
2020-11-10 12:28:35 +0100

    Add scroll to top button in Header

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
+// Librairies
+import React from 'react'
+// Style
 import './Header.css'
 
 const Header = () => {
+
+  // Functions
+  const scrollToPosition = (posX, posY) => {
+    window.scrollTo({
+      left: posX,
+      top: posY,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <header id="header">
 
       <img id="header__logo" src={require('../../assets/Logo.svg')} alt="logo de Mont d'Or"/>
       <input id="header__button" type="button" value="Rejoindre la communauté" />
+      <input id="header_scroll_top" type="button" value="▲" onClick={() => scrollToPosition(0, 0)} />
 
     </header>
   )
