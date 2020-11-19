b0ad9b58ccfbca2ea9e67d7a361a4b8556670c3f
Venesto <steven.pigere@gmail.com>
2020-11-19 11:38:55 +0100

    Setting up the foundation of the card Recipe on the homepage

--- front/src/components/TrendyRecipes/TrendyRecipes.js
+++ front/src/components/TrendyRecipes/TrendyRecipes.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import './TrendyRecipes.css'
 
+import img from '../../assets/smaug.jpg'
+
 class TrendyRecipes extends Component {
 
   state = {
@@ -24,14 +26,61 @@ class TrendyRecipes extends Component {
     return (
       <section id="trendyRecipes">
         <h2 className="trendyRecipes__titles">Recettes tendances de la Terre du Milieu</h2>
-        <div id="trendyRecipes__first">
-          <img src="../../assets/smaug.jpg" alt="recette"/>
-          <h3>La Douceur de Smaug</h3>
+
+        <div className="trendyRecipes__UI">
+          <div id="trendyRecipes__UI-firstCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-secondCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-thirdCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
         </div>
+
         <h2 className="trendyRecipes__titles">Recettes coup de coeur du chef personnel de Thranduil</h2>
-        <div></div>
+        
+        <div className="trendyRecipes__UI">
+          <div id="trendyRecipes__UI-fourthCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-fifthCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-sixthCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+        </div>
+
         <h2 className="trendyRecipes__titles">Dernières recettes</h2>
-        <div></div>
+
+        <div className="trendyRecipes__UI">
+          <div id="trendyRecipes__UI-seventhCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-eighteenthCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+
+          <div id="trendyRecipes__UI-nineteenthCard" className="trendyRecipes__UI-card">
+            <img src={img} alt="Recette"/>
+            <h3>La Douceur de Smaug</h3>
+          </div>
+        </div>
       </section>
     )
   }
