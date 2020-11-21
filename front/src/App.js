b257fbee6ce75ece1d1e1bd61e981d95f418013f
Kazemi4 <kevinpplle@gmail.com>
2020-11-21 15:38:12 +0100

    filtered applied

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,4 @@
-import React, { usteState, useEffect, useState } from 'react'
-import React from 'react'
+import React, { useState, useEffect,} from 'react'
 import { Switch, Route } from "react-router-dom"
 
 import Header from './components/Header/Header'
@@ -9,47 +8,28 @@ import RecipeSearchBar from './components/RecipeSearchBar/RecipeSearchBar'
 import TrendyRecipes from './components/TrendyRecipes/TrendyRecipes'
 import Recipes from './components/Recipes/Recipes'
 import Footer from './components/Footer/Footer'
+import RecipesList from './components/RecipesList/recipesList'
 
 import './App.css';
 
 
-const SearchPage = (props) => {
+
+const SearchRecipes = (props) => {
   const [input, setInput] = useState('');
-  const [recipesListDefault, setRecipesListDefault] = useState();
-  const [recipesList, setRecipesList] = useState();
-}
-
-
-const fetchData = async () => {
-  return await fetch('...')
-    .then(response => response.json())
-    .then(data => {
-      setRecipesList(data)
-      setRecipesListDefault(data)
-    });
+  const [recipesListDefault, setrecipesListDefault] = useState();
+  const [recipesList, setRecipes] = useState();
 }
 
 const updateInput = async (input) => {
-  const filtered = setRecipesListDefault.filter(recipes => {
+  const filtered = recipesListDefault.filter(recipes => {
     return recipes.name.toLowerCase().includes(input.toLowerCase())
   })
-  setInput(input)
-  setRecipesListDefault(filtered)
+  setInput(input);
+  setRecipesList(filtered);
 }
 
-useEffect(() => { fetchData() }, []);
+useEffect( () => {axiosData()}, []);
 
-return (
-    <>
-    <h1>Recipes List</h1>
-    <SearchBar
-      input={input}
-      onChange={updateInput
-        />
-        <RecipesList recipesList={recipesList} />
-    </>
-  );
-  }
 
 const App = () => {
   return (
@@ -57,7 +37,12 @@ const App = () => {
       <Header />
       <Nav />
       <Banner />
-      <RecipeSearchBar />
+      <RecipeSearchBar 
+      
+      />
+
+      <RecipesList  input={input}
+      onChange={updateInput}  />
       <Switch>
         <Route exact path='/' component={TrendyRecipes} />
         <Route path='/recipes' component={Recipes} />
--- front/src/components/RecipeSearchBar/RecipeSearchBar.js
+++ front/src/components/RecipeSearchBar/RecipeSearchBar.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './RecipeSearchBar.css'
 
-const RecipeSearchBar = () => {
+const RecipeSearchBar = ({input:keyword, onChange:setKeyword}) => {
+  const BarStyling = {width: "20rem", background:"#F2F1F9", border:"none", padding:"0.5rem"};
   return (
     <div id="searchBar">
-      <input type="text" id="searchBar__field" placeholder="Ingrédients, recettes..."></input>
+      <input style={BarStyling} key="random1" value={keyword} type="text" id="searchBar__field" placeholder={"Ingrédients, recettes..."} onChange={(e) => setKeyword(e.target.value)}></input>
       <input type="button" id="searchBar__button"></input>
     </div>
   )
