796080bc2ebfe85d78f20f8d2c0339d5e6e3b3ba
Jacqueline Nguyen <jacqueline.nguyen.41@gmail.com>
2020-12-02 15:28:46 +0100

    modified createStep to keep the number of the Step

--- front/src/components/RecipeForm/RecipeForm.js
+++ front/src/components/RecipeForm/RecipeForm.js
@@ -7,13 +7,15 @@ const RecipeForm = () => {
     const [prepTime, setPrepTime] = useState(undefined)
     const [cookTime, setCookTime] = useState(undefined)
 
-    const [number, setNumber] = useState(1)
     const [ingredient, setIngredient] = useState('Ingrédient')
     const [quantity, setQuantity] = useState('Quantité')
     const [unit, setUnit] = useState('Unité')
     const [listIngredient, setListIngredient] = useState([])
-    
 
+    const [step, setSteps] = useState('Ajouter une étape')
+    const [listSteps, setListSteps] = useState([])
+    const [nbStep, setNbStep] = useState(0)
+    
 
     useEffect(() => {
 
@@ -35,13 +37,40 @@ const RecipeForm = () => {
 
     const modifyIngredient = (index) => {
         const modification = listIngredient.filter((item, i) => i == index.i)
-        console.log(modification)
         setIngredient(modification[0].ingredient)
         setQuantity(modification[0].quantity)
         setUnit(modification[0].unit)
         deleteIngredient(index)
     }
 
+    const createStep = () => {
+        if (nbStep > 0 ) {
+            let number = nbStep
+            let newStep = {number,step}
+            setListSteps([...listSteps, newStep])
+            setNbStep(0)
+            setSteps('Ajouter une étape') 
+        } else {
+            let number = listSteps.length + 1
+            let newStep = {number,step}
+            setListSteps([...listSteps,newStep])
+            setSteps('Ajouter une étape') 
+        }
+               
+    }
+
+    const deleteStep = (index) => {
+    const newList = listSteps.filter((item, i) => i !== index.i)
+    setListSteps(newList)
+    }
+
+    const modifyStep = (index) => {
+        const modification = listSteps.filter((item, i) => i == index.i)
+        console.log(modification[0].number)
+        setNbStep(modification[0].number)
+        setSteps(modification[0].step)
+        deleteStep(index)
+    }
 
 
     return (
@@ -67,9 +96,9 @@ const RecipeForm = () => {
             { listIngredient.length > 0 ?
                 <div>{listIngredient.map((item, i) =>
                     <div>
-                        <input type='text' value={item.ingredient}></input>
-                        <input type='text' value={item.quantity}></input>
-                        <input type='text' value={item.unit} ></input>
+                        <li>{item.ingredient}</li>
+                        <li>{item.quantity}</li>
+                        <li>{item.unit}</li>
                         <input type='button' value='modifier' onClick={() => modifyIngredient({ i })}></input>
                         <input type='button' value='-' onClick={() => deleteIngredient({ i })}></input>
                     </div>
@@ -77,12 +106,29 @@ const RecipeForm = () => {
                 : ''
             }
 
-
             <section className='ingredient'>
                 <input type='text' value={ingredient} onChange={e => setIngredient(e.target.value)}></input>
                 <input type='text' value={quantity} onChange={e => setQuantity(Number(e.target.value))}></input>
                 <input type='text' value={unit} onChange={e => setUnit(e.target.value)}></input>
                 <input type='button' value='+' onClick={() => createIngredient()}></input>
+            </section>
+
+
+            { listSteps.length > 0 ?
+                <div>{listSteps.map((item, i) =>
+                    <div>
+                        <li>{item.number}</li>
+                        <li>{item.step}</li>
+                        <input type='button' value='modifier' onClick={() => modifyStep({ i })}></input>
+                        <input type='button' value='-'></input>
+                    </div>
+                )}</div>
+                : ''
+            }
+
+            <section className='step'>
+                <input type='text' value={step} onChange={e => setSteps(e.target.value)}></input>
+                <input type='button' value='+' onClick={() => createStep()}></input>
             </section>
 
 
