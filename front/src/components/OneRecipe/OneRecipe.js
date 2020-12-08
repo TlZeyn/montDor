bda2c37bc5c715605851de61ebcddb5ab198537f
Cyril Carral <cyril.carral85@gmail.com>
2020-12-09 00:28:57 +0100

    CSS done, Bem method applied

--- front/src/components/OneRecipe/OneRecipe.js
+++ front/src/components/OneRecipe/OneRecipe.js
@@ -3,19 +3,21 @@ import axios from 'axios';
 
 import './OneRecipe.css'
 
-import img from '../../assets/smaug.jpg'
 import img2 from '../../assets/avatar.jpeg'
+import img3 from '../../assets/chief.jpeg'
 
 const OneRecipe = () => {
 
+    
+
     return(
 
-      <section className="master.container">
+      <section className="oneRecipe">
 
-         <div className="container">
-            <div className="recipe-header__img"></div>
-            <img className="recipe-header__img2" src={img2} alt="avatar"/>
-            <h2 className="recipe-header__title">Nom de la recette</h2>
+         <div className="oneRecipe__container">
+            <div className="oneRecipe__container-header--img"></div>
+            <img className="oneRecipe__container-header--img2" src={img2} alt="avatar"/>
+            <h2 className="oneRecipe__container-header--title">Nom de la recette</h2>
             <div className="recommandations">
                <ul>
                   <li>Nombre d'aventuriers : 6</li>
@@ -25,10 +27,10 @@ const OneRecipe = () => {
              </div>
             <div className="describe-part">
                <div className="ingredients">
-                  <h3>Ingrédients :</h3>
-                  <p>1er ingrédient</p>
-                  <p>2eme ingrédient</p>
-                  <p>3eme ingrédient</p>
+                  <h3>Ingrédients</h3>
+                  <p>- 1er ingrédient</p>
+                  <p>- 2eme ingrédient</p>
+                  <p>- 3eme ingrédient</p>
                </div>
                <div className="steps">
                   <h3>Préparation</h3>
@@ -41,6 +43,13 @@ const OneRecipe = () => {
                   <img src="https://via.placeholder.com/150" />
                   <img src="https://via.placeholder.com/150" />
                   <img src="https://via.placeholder.com/150" />
+               </div>
+            </div>
+            <div className="chiefTrick">
+               <img src={img3} alt="chief"/>
+               <div className="chiefTrick__text">
+                  <h4>L'astuce du chef Sam :</h4><br></br>
+                  <p>" Je recommande d'accompagner cette recette avec de bonnes patates ! "</p>
                </div>
             </div>
          </div>
