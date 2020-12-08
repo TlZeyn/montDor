f4b6691853405d9796ea73aa99b5e62f14aa3618
Cyril Carral <cyril.carral85@gmail.com>
2020-12-08 01:31:29 +0100

    recipe image and avatar are in place, recommandations too, CSS in progress with bem method

--- front/src/components/OneRecipe/OneRecipe.js
+++ front/src/components/OneRecipe/OneRecipe.js
@@ -9,49 +9,42 @@ import img2 from '../../assets/avatar.jpeg'
 const OneRecipe = () => {
 
     return(
-       <section className="master.container">
 
-       <div className="container">
-           <div className="recipe-header">
-               <div>
-                  <img className="recipe-header__img" src={img} alt="recette"/>
-               </div>
-               <div>
-                  <img className="recipe-header__img2" src={img2} alt="avatar"/>
-               </div>
-               <h1 className="recipe-header__title">Nom de la recette</h1>
-           </div>
-           <div className="recommandations">
+      <section className="master.container">
+
+         <div className="container">
+            <div className="recipe-header__img"></div>
+            <img className="recipe-header__img2" src={img2} alt="avatar"/>
+            <h2 className="recipe-header__title">Nom de la recette</h2>
+            <div className="recommandations">
                <ul>
-                   <li>Nombre d'aventuriers : 6</li>
-                   <li>Temps de préparation : 15 min</li>
-                   <li>temps de cuisson : 35 min</li>
-               </ul>
-           </div>
-           <div className="describe-part">
-                <div className="ingredients">
-                   <h2>Ingrédients :</h2>
-                   <p>1er ingrédient</p>
-                   <p>2eme ingrédient</p>
-                   <p>3eme ingrédient</p>
-                </div>
-                <div className="steps">
-                   <h2>Préparation</h2>
-                   <p>Je fais d'abord ça</p>
-                   <p>Puis ça...</p>
-                   <p>Puis je finis par ça</p>
-                </div>
-                <div className="suggestion">
-                   <h2>Recettes suggérées</h2>
-                   <img src="https://via.placeholder.com/150" />
-                   <img src="https://via.placeholder.com/150" />
-                   <img src="https://via.placeholder.com/150" />
-                </div>
-           </div>
-           
-
-       </div>
-       </section> 
+                  <li>Nombre d'aventuriers : 6</li>
+                  <li>Temps de préparation : 15 min</li>
+                  <li>Temps de cuisson : 35 min</li>
+               </ul>         
+             </div>
+            <div className="describe-part">
+               <div className="ingredients">
+                  <h3>Ingrédients :</h3>
+                  <p>1er ingrédient</p>
+                  <p>2eme ingrédient</p>
+                  <p>3eme ingrédient</p>
+               </div>
+               <div className="steps">
+                  <h3>Préparation</h3>
+                  <p>Je fais d'abord ça</p>
+                  <p>Puis ça...</p>
+                  <p>Puis je finis par ça</p>
+               </div>
+               <div className="suggestion">
+                  <h3>Recettes suggérées</h3>
+                  <img src="https://via.placeholder.com/150" />
+                  <img src="https://via.placeholder.com/150" />
+                  <img src="https://via.placeholder.com/150" />
+               </div>
+            </div>
+         </div>
+      </section> 
   );
 };
 
