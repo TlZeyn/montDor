b856c6fefbc11e6294f2c242fa0fb95d5a01718a
Jacqueline Nguyen <jacqueline.nguyen.41@gmail.com>
2020-12-20 21:30:15 +0100

    added comments to start createRecipe

--- back/routes/createRecipe.js
+++ back/routes/createRecipe.js
@@ -5,6 +5,21 @@ const connection = require('../conf')
 router.post('/', function (req, res) {
     console.log(req.body)
     console.log(req.body.listSteps)
+    connection.query(
+        'SELECT rcp_title AS title, rcp_nbEater AS nbEater, rcp_photo AS photo FROM t_recipe_rcp ORDER BY rcp_creationDate LIMIT 3', (err, results) => {
+            if (err) {
+                res.status(500).send('Erreur lors de la récupération des recettes');
+            } else {
+                res.json(results);
+            }
+        })
+
+    //insert ingredients => check if in db or not //get id -> trigger
+    //insert data in recipe
+    //insert data in joint table recipe igr
+    //insert data in step
+    // insert data in joint table recipe step
+
 
 });
 
